e61d61dae7765cc167815d0a8a9e05367924fb7b
SurAyush <ayushsur26@gmail.com>
2025-11-21 17:35:57 +0530

    changed checkpoint saving strategy using accelerate hooks + reformatted codebase using ruff

--- training/configuration.py
+++ training/configuration.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class TrainingConfig:
     pretrained_model_name_or_path: str = "stable-diffusion-v1-5/stable-diffusion-v1-5"
@@ -12,17 +13,17 @@ class TrainingConfig:
     train_batch_size: int = 8
     gradient_accumulation_steps: int = 1
     max_train_steps: int = 15000
-    learning_rate: int = 1e-04 
-    max_grad_norm: int = 1 
-    lr_scheduler: str = "cosine" 
-    lr_warmup_steps: int = 0 
+    learning_rate: int = 1e-04
+    max_grad_norm: int = 1
+    lr_scheduler: str = "cosine"
+    lr_warmup_steps: int = 0
     push_to_hub: bool = True
     hub_model_id: str = "StableEmote-lora"
-    report_to: str= "wanddb"
-    logging_dir: str ="logs"
-    checkpointing_steps: int =500 
-    validation_prompt: str ="emoji of robot face"
-    num_validation_images: int = 4 
+    report_to: str = "wanddb"
+    logging_dir: str = "logs"
+    checkpointing_steps: int = 500
+    validation_prompt: str = "emoji of robot face"
+    num_validation_images: int = 4
     validation_epochs: int = 1
     image_interpolation_mode: str = "lanczos"
     center_crop: bool = False
@@ -36,4 +37,3 @@ class TrainingConfig:
     xformers_memory_efficient_attention: bool = True
     gradient_checkpointing: bool = False
     seed: int = 42
-
--- training/train.py
+++ training/train.py
@@ -5,7 +5,6 @@ from save_model import save_model_card
 import logging
 import math
 import os
-from contextlib import nullcontext
 from pathlib import Path
 
 import datasets
@@ -19,20 +18,36 @@ from datasets import load_dataset
 from huggingface_hub import create_repo, upload_folder
 from packaging import version
 from peft import LoraConfig
-from peft.utils import get_peft_model_state_dict
+from peft.utils import get_peft_model_state_dict, set_peft_model_state_dict
 from torchvision import transforms
 from tqdm.auto import tqdm
 from transformers import CLIPTextModel, CLIPTokenizer
 
 import diffusers
-from diffusers import AutoencoderKL, DDPMScheduler, DiffusionPipeline, StableDiffusionPipeline, UNet2DConditionModel
+from diffusers import (
+    AutoencoderKL,
+    DDPMScheduler,
+    DiffusionPipeline,
+    StableDiffusionPipeline,
+    UNet2DConditionModel,
+)
 from diffusers.optimization import get_scheduler
 from diffusers.training_utils import cast_training_params
 from diffusers.utils import check_min_version, convert_state_dict_to_diffusers
 from diffusers.utils.import_utils import is_xformers_available
 from diffusers.utils.torch_utils import is_compiled_module
 
-logger = get_logger(__name__, log_level="INFO")       # doesn't log debug messages
+# compatible with both older and new version of diffusers -- more robust approach
+try:
+    from diffusers.loaders.lora_conversion_utils import convert_unet_state_dict_to_peft
+except ImportError:
+    try:
+        from diffusers.loaders.peft import convert_unet_state_dict_to_peft
+    except ImportError:
+        from diffusers.loaders.lora import convert_unet_state_dict_to_peft
+
+
+logger = get_logger(__name__, log_level="INFO")  # doesn't log debug messages
 
 # Will error if the minimal version of diffusers necessary is not installed
 check_min_version("0.36.0.dev0")
@@ -43,7 +58,9 @@ def train():
 
     logging_dir = Path(config.output_dir, config.logging_dir)
 
-    accelerator_project_config = ProjectConfiguration(project_dir=config.output_dir, logging_dir=logging_dir)
+    accelerator_project_config = ProjectConfiguration(
+        project_dir=config.output_dir, logging_dir=logging_dir
+    )
 
     accelerator = Accelerator(
         gradient_accumulation_steps=config.gradient_accumulation_steps,
@@ -64,31 +81,41 @@ def train():
         datasets.utils.logging.set_verbosity_warning()
         transformers.utils.logging.set_verbosity_warning()
         diffusers.utils.logging.set_verbosity_info()
-    else:   # other process with only log errors
+    else:  # other process with only log errors
         datasets.utils.logging.set_verbosity_error()
         transformers.utils.logging.set_verbosity_error()
         diffusers.utils.logging.set_verbosity_error()
-
 
     set_seed(config.seed if config.seed is not None else 42)
 
     # Handle the repository creation
     if accelerator.is_main_process:
-
         if config.output_dir is not None:
             os.makedirs(config.output_dir, exist_ok=True)
 
         if config.push_to_hub:
             repo_id = create_repo(
-                repo_id=config.hub_model_id or Path(config.output_dir).name, exist_ok=True, token=config.hub_token
+                repo_id=config.hub_model_id or Path(config.output_dir).name,
+                exist_ok=True,
+                token=config.hub_token,
             ).repo_id
 
     # Load scheduler, tokenizer and models.
-    noise_scheduler = DDPMScheduler.from_pretrained(config.pretrained_model_name_or_path, subfolder="scheduler")   # cosine scehdule
-    tokenizer = CLIPTokenizer.from_pretrained(config.pretrained_model_name_or_path, subfolder="tokenizer")
-    text_encoder = CLIPTextModel.from_pretrained(config.pretrained_model_name_or_path, subfolder="text_encoder")
-    vae = AutoencoderKL.from_pretrained(config.pretrained_model_name_or_path, subfolder="vae")
-    unet = UNet2DConditionModel.from_pretrained(config.pretrained_model_name_or_path, subfolder="unet")
+    noise_scheduler = DDPMScheduler.from_pretrained(
+        config.pretrained_model_name_or_path, subfolder="scheduler"
+    )  # cosine scehdule
+    tokenizer = CLIPTokenizer.from_pretrained(
+        config.pretrained_model_name_or_path, subfolder="tokenizer"
+    )
+    text_encoder = CLIPTextModel.from_pretrained(
+        config.pretrained_model_name_or_path, subfolder="text_encoder"
+    )
+    vae = AutoencoderKL.from_pretrained(
+        config.pretrained_model_name_or_path, subfolder="vae"
+    )
+    unet = UNet2DConditionModel.from_pretrained(
+        config.pretrained_model_name_or_path, subfolder="unet"
+    )
 
     # freeze parameters of models: LoRA
     unet.requires_grad_(False)
@@ -114,7 +141,7 @@ def train():
     vae.to(accelerator.device, dtype=weight_dtype)
     text_encoder.to(accelerator.device, dtype=weight_dtype)
 
-    unet.add_adapter(unet_lora_config)        # add LoRA layers to unet
+    unet.add_adapter(unet_lora_config)  # add LoRA layers to unet
     if config.mixed_precision == "fp16":
         cast_training_params(unet, dtype=torch.float32)
 
@@ -130,8 +157,9 @@ def train():
                 )
             unet.enable_xformers_memory_efficient_attention()
         else:
-            raise ValueError("xformers is not available. Make sure it is installed correctly")
-
+            raise ValueError(
+                "xformers is not available. Make sure it is installed correctly"
+            )
 
     lora_layers = filter(lambda p: p.requires_grad, unet.parameters())
 
@@ -156,18 +184,27 @@ def train():
     image_column = dataset_columns[0]
     caption_column = dataset_columns[1]
 
-
     # Get the specified interpolation method from the config
-    interpolation = getattr(transforms.InterpolationMode, config.image_interpolation_mode.upper(), None)
+    interpolation = getattr(
+        transforms.InterpolationMode, config.image_interpolation_mode.upper(), None
+    )
     if interpolation is None:
-        raise ValueError(f"Unsupported interpolation mode {config.image_interpolation_mode}.")
+        raise ValueError(
+            f"Unsupported interpolation mode {config.image_interpolation_mode}."
+        )
 
     # Data preprocessing transformations
     train_transforms = transforms.Compose(
         [
-            transforms.Resize(config.resolution, interpolation=interpolation),  # Use dynamic interpolation method
-            transforms.CenterCrop(config.resolution) if config.center_crop else transforms.RandomCrop(config.resolution),
-            transforms.RandomHorizontalFlip() if config.random_flip else transforms.Lambda(lambda x: x),
+            transforms.Resize(
+                config.resolution, interpolation=interpolation
+            ),  # Use dynamic interpolation method
+            transforms.CenterCrop(config.resolution)
+            if config.center_crop
+            else transforms.RandomCrop(config.resolution),
+            transforms.RandomHorizontalFlip()
+            if config.random_flip
+            else transforms.Lambda(lambda x: x),
             transforms.ToTensor(),
             transforms.Normalize([0.5], [0.5]),
         ]
@@ -182,14 +219,20 @@ def train():
         captions = []
         for caption in examples[caption_column]:
             if isinstance(caption, str):
-                captions.append("An emoji of " + caption)     # prefixing captions with "An emoji of "
+                captions.append(
+                    "An emoji of " + caption
+                )  # prefixing captions with "An emoji of "
             else:
                 raise ValueError(
                     f"Caption column `{caption_column}` should contain strings"
                 )
 
         inputs = tokenizer(
-            captions, max_length=tokenizer.model_max_length, padding="max_length", truncation=True, return_tensors="pt"
+            captions,
+            max_length=tokenizer.model_max_length,
+            padding="max_length",
+            truncation=True,
+            return_tensors="pt",
         )
         return inputs.input_ids
 
@@ -205,7 +248,6 @@ def train():
         input_ids = torch.stack([example["input_ids"] for example in examples])
         return {"pixel_values": pixel_values, "input_ids": input_ids}
 
-
     with accelerator.main_process_first():
         # Set the training transforms
         train_dataset = dataset["train"].with_transform(preprocess_train)
@@ -219,8 +261,67 @@ def train():
         num_workers=config.dataloader_num_workers,
     )
 
+    def save_model_hook(models, weights, output_dir):
+        if accelerator.is_main_process:
+            unet_lora_layers_to_save = None
+
+            for model in models:
+                if isinstance(model, type(unwrap_model(unet))):
+                    unet_lora_layers_to_save = get_peft_model_state_dict(model)
+                else:
+                    raise ValueError(f"Unexpected save model: {model.__class__}")
+
+                # make sure to pop weight so that corresponding model is not saved again
+                weights.pop()
+
+            StableDiffusionPipeline.save_lora_weights(
+                save_directory=output_dir,
+                unet_lora_layers=unet_lora_layers_to_save,
+                safe_serialization=True,
+            )
+
+    def load_model_hook(models, input_dir):
+        unet_ = None
+
+        while len(models) > 0:
+            model = models.pop()
+            if isinstance(model, type(unwrap_model(unet))):
+                unet_ = model
+            else:
+                raise ValueError(f"unexpected save model: {model.__class__}")
+
+        # lora_state_dict = StableDiffusionPipeline.lora_state_dict(input_dir)
+        lora_state_dict, network_alphas = StableDiffusionPipeline.lora_state_dict(
+            input_dir
+        )
+
+        unet_state_dict = {
+            f"{k.replace('unet.', '')}": v
+            for k, v in lora_state_dict.items()
+            if k.startswith("unet.")
+        }
+        unet_state_dict = convert_unet_state_dict_to_peft(unet_state_dict)
+        incompatible_keys = set_peft_model_state_dict(
+            unet_, unet_state_dict, adapter_name="default"
+        )
+
+        if incompatible_keys is not None:
+            # check only for unexpected keys
+            unexpected_keys = getattr(incompatible_keys, "unexpected_keys", None)
+            if unexpected_keys:
+                logger.warning(
+                    f"Loading adapter weights from state_dict led to unexpected keys not found in the model: "
+                    f" {unexpected_keys}. "
+                )
+
+        # Make sure the trainable params are in float32
+        if config.mixed_precision in ["fp16", "bf16"]:
+            cast_training_params([unet_], dtype=torch.float32)
+
     num_warmup_steps_for_scheduler = config.lr_warmup_steps * accelerator.num_processes
-    num_training_steps_for_scheduler = config.max_train_steps * accelerator.num_processes
+    num_training_steps_for_scheduler = (
+        config.max_train_steps * accelerator.num_processes
+    )
 
     lr_scheduler = get_scheduler(
         config.lr_scheduler,
@@ -234,13 +335,24 @@ def train():
         unet, optimizer, train_dataloader, lr_scheduler
     )
 
-    num_update_steps_per_epoch = math.ceil(len(train_dataloader) / config.gradient_accumulation_steps)
-    config.num_train_epochs = math.ceil(config.max_train_steps / num_update_steps_per_epoch)
+    accelerator.register_save_state_pre_hook(save_model_hook)
+    accelerator.register_load_state_pre_hook(load_model_hook)
+
+    num_update_steps_per_epoch = math.ceil(
+        len(train_dataloader) / config.gradient_accumulation_steps
+    )
+    config.num_train_epochs = math.ceil(
+        config.max_train_steps / num_update_steps_per_epoch
+    )
 
     if accelerator.is_main_process:
         accelerator.init_trackers("text2image-fine-tune", config=vars(config))
 
-    total_batch_size = config.train_batch_size * accelerator.num_processes * config.gradient_accumulation_steps
+    total_batch_size = (
+        config.train_batch_size
+        * accelerator.num_processes
+        * config.gradient_accumulation_steps
+    )
 
     log_init(logger, len(train_dataset), config, total_batch_size)
     global_step = 0
@@ -263,26 +375,17 @@ def train():
             accelerator.load_state(os.path.join(config.output_dir, path))
             global_step = int(path.split("-")[1])
 
-            # Loading LoRA weights
-            unet_lora_path = os.path.join(config.output_dir, path, "pytorch_lora_weights.safetensors") 
-            if os.path.exists(unet_lora_path): 
-                # Import here to avoid unnecessary dependency if not resuming from checkpoint 
-                from safetensors.torch import load_file 
-                lora_state_dict = load_file(unet_lora_path) 
-                unwrapped_unet = unwrap_model(unet) 
-                unwrapped_unet.load_state_dict(lora_state_dict, strict=False) 
-                logger.info(f"Successfully loaded LoRA weights from {unet_lora_path}") 
-            else: 
-                # If LoRA weights are not found, throw a warning and continue without loading them 
-                logger.warning( f"LoRA weights not found at {unet_lora_path}. Continuing with optimizer/scheduler state only." )
-
             initial_global_step = global_step
             first_epoch = global_step // num_update_steps_per_epoch
     else:
         initial_global_step = 0
 
-
-    progress_bar = tqdm(range(0, config.max_train_steps),initial=initial_global_step,desc="Steps",disable=not accelerator.is_local_main_process)
+    progress_bar = tqdm(
+        range(0, config.max_train_steps),
+        initial=initial_global_step,
+        desc="Steps",
+        disable=not accelerator.is_local_main_process,
+    )
 
     # ------------TRAINING LOOP------------
     for epoch in range(first_epoch, config.num_train_epochs):
@@ -290,35 +393,49 @@ def train():
         train_loss = 0.0
         for _, batch in enumerate(train_dataloader):
             with accelerator.accumulate(unet):
-
                 # Convert images to latent space
-                latents = vae.encode(batch["pixel_values"].to(dtype=weight_dtype)).latent_dist.sample()
+                latents = vae.encode(
+                    batch["pixel_values"].to(dtype=weight_dtype)
+                ).latent_dist.sample()
                 latents = latents * vae.config.scaling_factor
 
                 # Sample noise that we'll add to the latents
                 noise = torch.randn_like(latents)
 
                 bsz = latents.shape[0]
-                timesteps = torch.randint(0, noise_scheduler.config.num_train_timesteps, (bsz,), device=latents.device)
+                timesteps = torch.randint(
+                    0,
+                    noise_scheduler.config.num_train_timesteps,
+                    (bsz,),
+                    device=latents.device,
+                )
                 timesteps = timesteps.long()
 
                 noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps)
-                encoder_hidden_states = text_encoder(batch["input_ids"], return_dict=False)[0]
+                encoder_hidden_states = text_encoder(
+                    batch["input_ids"], return_dict=False
+                )[0]
 
                 if noise_scheduler.config.prediction_type == "epsilon":
                     target = noise
                 elif noise_scheduler.config.prediction_type == "v_prediction":
                     target = noise_scheduler.get_velocity(latents, noise, timesteps)
                 else:
-                    raise ValueError(f"Unknown prediction type {noise_scheduler.config.prediction_type}")
+                    raise ValueError(
+                        f"Unknown prediction type {noise_scheduler.config.prediction_type}"
+                    )
 
                 # Predict the noise residual and compute loss
-                model_pred = unet(noisy_latents, timesteps, encoder_hidden_states, return_dict=False)[0]
+                model_pred = unet(
+                    noisy_latents, timesteps, encoder_hidden_states, return_dict=False
+                )[0]
 
                 loss = F.mse_loss(model_pred.float(), target.float(), reduction="mean")
 
                 # Gather the losses across all processes for logging (if we use distributed training).
-                avg_loss = accelerator.gather(loss.repeat(config.train_batch_size)).mean()
+                avg_loss = accelerator.gather(
+                    loss.repeat(config.train_batch_size)
+                ).mean()
                 train_loss += avg_loss.item() / config.gradient_accumulation_steps
 
                 # Backpropagate
@@ -339,30 +456,26 @@ def train():
 
                 if global_step % config.checkpointing_steps == 0:
                     if accelerator.is_main_process:
-                        save_path = os.path.join(config.output_dir, f"checkpoint-{global_step}")
+                        save_path = os.path.join(
+                            config.output_dir, f"checkpoint-{global_step}"
+                        )
                         accelerator.save_state(save_path)
-
-                        unwrapped_unet = unwrap_model(unet)
-                        unet_lora_state_dict = convert_state_dict_to_diffusers(
-                            get_peft_model_state_dict(unwrapped_unet)
-                        )
-
-                        StableDiffusionPipeline.save_lora_weights(
-                            save_directory=save_path,
-                            unet_lora_layers=unet_lora_state_dict,
-                            safe_serialization=True,
-                        )
-
                         logger.info(f"Saved state to {save_path}")
 
-            logs = {"step_loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0]}
+            logs = {
+                "step_loss": loss.detach().item(),
+                "lr": lr_scheduler.get_last_lr()[0],
+            }
             progress_bar.set_postfix(**logs)
 
             if global_step >= config.max_train_steps:
                 break
 
         if accelerator.is_main_process:
-            if config.validation_prompt is not None and epoch % config.validation_epochs == 0:
+            if (
+                config.validation_prompt is not None
+                and epoch % config.validation_epochs == 0
+            ):
                 # create pipeline
                 pipeline = DiffusionPipeline.from_pretrained(
                     config.pretrained_model_name_or_path,
@@ -380,7 +493,9 @@ def train():
         unet = unet.to(torch.float32)
 
         unwrapped_unet = unwrap_model(unet)
-        unet_lora_state_dict = convert_state_dict_to_diffusers(get_peft_model_state_dict(unwrapped_unet))
+        unet_lora_state_dict = convert_state_dict_to_diffusers(
+            get_peft_model_state_dict(unwrapped_unet)
+        )
         StableDiffusionPipeline.save_lora_weights(
             save_directory=config.output_dir,
             unet_lora_layers=unet_lora_state_dict,
