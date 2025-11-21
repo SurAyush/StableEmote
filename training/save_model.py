6a784906cf4631f341a8af371e1b197a8f993a55
SurAyush <ayushsur26@gmail.com>
2025-11-21 17:35:57 +0530

    changed checkpoint saving strategy using accelerate hooks + reformatted codebase using ruff

--- training/save_model.py
+++ training/save_model.py
@@ -3,8 +3,7 @@ from diffusers.utils.hub_utils import load_or_create_model_card, populate_model_
 
 
 def save_model_card(repo_id: str, base_model: str = None, repo_folder: str = None):
-
-    model_description = f"""
+    model_description = """
         # LoRA text2image fine-tuning of stable-diffusion-1.5
         These are LoRA adaption weights for stable-diffusion-1.5. The weights were fine-tuned on the valhalla/emoji-dataset dataset.
         Refer to the GitHub Repo for more information: https://github.com/SurAyush/StableEmote
@@ -18,10 +17,9 @@ def save_model_card(repo_id: str, base_model: str = None, repo_folder: str = Non
         inference=True,
     )
 
-    tags = ["text-to-image","diffusers","stable-diffusion","lora"]
+    tags = ["text-to-image", "diffusers", "stable-diffusion", "lora"]
 
     model_card = populate_model_card(model_card, tags=tags)
 
     # Save the model card to the repo folder
     model_card.save(os.path.join(repo_folder, "README.md"))
-
